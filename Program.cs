97e9a251abf08155140dcd730e75ddf11e67ada8
deividneres <116382109+deividneres@users.noreply.github.com>
2023-07-03 23:00:46 -0300

    Segundo Exercicio em C#

--- Program.cs
+++ Program.cs
@@ -1,16 +1,10 @@
-﻿namespace OlaJoao
+﻿namespace OlaMundo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Qual é o seu nome? ");
-            string nome = Console.ReadLine();
-
-            Console.WriteLine("Olá " +  nome + ", é um prazer te conhecer" );
-
-            
-
+            Console.WriteLine("Hello, World!");
         }
     }
 }
