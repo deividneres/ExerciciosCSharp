3cf7968652f03bbfea08f2bbd46b78c343494a3d
deividneres <116382109+deividneres@users.noreply.github.com>
2023-07-03 22:16:35 -0300

    Primeiro Exercicio em C#

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace OlaJoao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é o seu nome? ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Olá " +  nome + ", é um prazer te conhecer" );
+
+            
+
+        }
+    }
+}
